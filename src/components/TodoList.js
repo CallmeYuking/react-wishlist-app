0232bd7204d9359597e15d617f4b2eda4cc6fe0b
Yuking Quan <yuking@Yukings-MacBook-Pro.local>
2021-06-06 15:09:02 -0700

    initial file set up. Creating components file in src folder and set up todolist.js file.

---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function TodoList() {
+    return (
+      <div className="body">
+        <div className="header text-center">
+            <h1>WISHLIST</h1>
+            <h5>{}JOBS</h5>
+            <button className="btn btn-light btn-lg mt-5"> + </button>
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+    )
+}
+
+export default TodoList
